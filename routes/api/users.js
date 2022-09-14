c9e5d70978e08960b6b8f98097076e65910ce69b
Yuheng Lu <lyh2002329@gmail.com>
2022-09-14 00:14:36 -0400

    add posts rount

--- routes/api/users.js
+++ routes/api/users.js
@@ -22,37 +22,30 @@ router.post('/',
             return res.status(400).json({ errors: errors.array() });
         }
         const { name, email, password } = req.body;
-
         try {
             let user = await User.findOne({ email });
             if (user) {
                 return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
             }
-
             const avatar = gravatar.url(email, {
                 s: '200',
                 r: 'pg',
                 d: 'mm'
             })
-
             user = new User({
                 name,
                 email,
                 avatar,
                 password
             });
-
             const salt = await bcrypt.genSalt(10);
             user.password = await bcrypt.hash(password, salt);
-
             await user.save();
-
             const payload = {
                 user: {
                     id: user.id
                 }
             }
-
             jwt.sign(
                 payload, config.get('jwtSecret'),
                 { expiresIn: 360000 },
@@ -64,9 +57,6 @@ router.post('/',
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server Error');
-        }
-
-
+        } s
     });
-
 module.exports = router;
